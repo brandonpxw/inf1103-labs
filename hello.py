9a56450dac5d2e43f975594a3bb74f05d4bd41ca
Your Name <your_email@example.com>
2026-09-04 14:59:18 +0800

    5th Post

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 # print("============================================")
 # print("Welcome here")
 # print("My first post!")
-# print("============================================")
+print("============================================")
 
 # username = "cool_creator"
 # bio = "Fun Blogger"
